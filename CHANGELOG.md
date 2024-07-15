c533cae740e6bb5da278a6dd52d993863ade88aa
goncaloasimoes <32324608+goncaloasimoes@users.noreply.github.com>
2024-07-15 20:46:31 +0100

    Fix build assets

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Remove development assets and use production build assets instead.
+
 ### [0.4.3] - 2024-07-12
 
 ### Added
--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-primitives'), 'version' => 'ea57c9f057488543e6e0');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-primitives'), 'version' => '8033f71649903084ff7d');
