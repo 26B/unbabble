c8a7ddbafdf5f4fe41cb4ad5ddc333083ef8e18e
goncaloasimoes <32324608+goncaloasimoes@users.noreply.github.com>
2024-07-18 17:01:04 +0100

    Rebuild assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-edit-post', 'wp-plugins', 'wp-primitives'), 'version' => '7b47c86697b71ff86a8b');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-edit-post', 'wp-plugins', 'wp-primitives'), 'version' => 'b8bba0220923023d04d3');
